77d8fe377e8ebe8690773d14848261e1a3da26d9
Yuriy <zdorenkoviti@gmail.com>
2021-01-10 18:48:53 +0200

    second

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.post("/user", jsonParser, function (request, response) {
     console.log(request.body);
     if(!request.body) return response.sendStatus(400);
 
+
     response.json(request.body); // отправляем пришедший ответ обратно
 
 });
